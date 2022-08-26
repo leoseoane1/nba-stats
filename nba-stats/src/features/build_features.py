6aca2d52bb6048f93ed2d86a9c8b27363d0429e7
Leonardo Seoane <leoseoane1@live.com>
2022-08-26 14:56:03 -0400

    added build features for easier viewing

--- nba-stats/src/features/build_features.py
+++ nba-stats/src/features/build_features.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import os
-
+'''
 raw_stats=pd.read_csv(r'C:/Users/leose/nba/nba-stats/data/raw/ASA_All_NBA_Raw_Data.csv')
 team_raptor=pd.read_csv(r'C:/Users/leose/nba/nba-stats/data/raw/modern_RAPTOR_by_team.csv')
 player_raptor=pd.read_csv(r'C:/Users/leose/nba/nba-stats/data/raw/modern_RAPTOR_by_player.csv')
@@ -8,3 +8,18 @@ player_raptor=pd.read_csv(r'C:/Users/leose/nba/nba-stats/data/raw/modern_RAPTOR_
 raw_stats['game_date']=pd.to_datetime(raw_stats['game_date'])
 
 raw_stats.to_csv('C:/Users/leose/nba/nba-stats/src/data/modified_data.csv')
+'''
+
+raw_stats=pd.read_csv('C:/Users/leose/nba/nba-stats/src/data/wins_modified_data.csv')
+categorical_columns = ['H_A']
+for column in categorical_columns:
+        tempdf = pd.get_dummies(raw_stats[column], prefix=column)
+        raw_stats = pd.merge(
+            left=raw_stats,
+            right=tempdf,
+            left_index=True,
+            right_index=True,
+        )
+        raw_stats = raw_stats.drop(columns=column)
+
+raw_stats.to_csv('C:/Users/leose/nba/nba-stats/src/data/wins_modified_data.csv')
